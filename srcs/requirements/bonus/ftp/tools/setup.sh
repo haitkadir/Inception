97e490177da3bcc3cfb489e17d3dc44539773ca2
Hssain Aitkadir <hssain.aitkadir@gmail.com>
2023-04-16 00:42:54 +0000

    Update some small changes on ftp part

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -2,6 +2,7 @@
 
 useradd -ms /bin/bash $FTP_USER
 echo "$FTP_USER:$FTP_PASS" | chpasswd
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
 # echo "local_root=/home/$FTP_USER/ftp" >> /etc/vsftpd.conf
 # echo "write_enable=YES" >> /etc/vsftpd.conf 
@@ -16,8 +17,14 @@ echo "$FTP_USER:$FTP_PASS" | chpasswd
 mkdir -p /var/run/vsftpd/empty # Default directory
 
 mkdir -p /home/$FTP_USER/ftp 
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
-chmod 755 /home/$FTP_USER/ftp
+chown -R nobody:nogroup /home/$FTP_USER/ftp
+# chmod 755 /home/$FTP_USER/ftp
+
+# Sets the owner of the /home/ata/ftp directory to be nobody.
+# sudo chown nobody:nogroup /home/ata/ftp
+# Ensures that only the owner (your FTP user) 
+# has write(w) access(a) to the /home/ata/ftp directory.
+sudo chmod a-w /home/$FTP_USER/ftp
 
 
 exec "$@"
